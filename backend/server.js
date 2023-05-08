dc572b80f681bd46fc8cc6182aeddacbd5008f98
Angelo Boggio <gelobogx@gmail.com>
2023-05-08 16:46:30 -0400

    Wrapping up the frontend/backend connectivity for the signup/signin page

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cors = require('cors');
 const session = require('express-session');
 const {User} = require('./models/user');
 const initializePassport = require('./passport-config');
 const userController = require('./controllers/userController');
+
+const PORT = process.env.PORT || 3001;
 
 
 // initialize passport
 const app = express();
 initializePassport(passport);
 app.use(passport.initialize());
-
+app.use(cors());
 
 // adding my mongodb connection string
 const dbURI = 'mongodb+srv://gelobogx:vtrKWJYwO3naNcer@myfirstcluster.ohegfcw.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then((result) => {
         console.log('Connected to MongoDB');
-        app.listen(3001, () =>{
-            console.log('Server started on port 3002');
+        app.listen(PORT, () =>{
+            console.log(`Server started on port ${PORT}`);
         });
     })
     .catch((err) => console.log(err));
